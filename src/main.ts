634aa99ae6926389f37e4ed739b67cdd49f8a097
muhamadgibran16 <muhamadgibran2333@gmail.com>
2023-08-23 22:14:46 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
 
-  await app.listen(parseInt(process.env.PORT) || 3000);
+  const port = process.env.PORT;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
